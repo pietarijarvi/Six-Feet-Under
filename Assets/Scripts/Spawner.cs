ce56e27bb14fff6d05b97b27ded427d66366990c
Marko Seppänen <marko.p.seppanen@gmail.com>
2018-11-20 13:18:29 +0200

    Material added

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,19 +5,14 @@ using UnityEngine;
 public class Spawner : MonoBehaviour {
 
 	public GameObject[] obj;
-	//public Transform target;
-	Camera cam;
-	// Use this for initialization
 	void Start () {
-		
-		cam = GetComponent<Camera>();
-		Spawn();
-		InvokeRepeating ("Spawn", 2f, 0.7f);
+//		Spawn();
+		InvokeRepeating ("Spawn", 2f, 1f);
 	}
 	
 	void Spawn()
 	{
-		Vector3 viewpos = cam.WorldToViewportPoint(new Vector3());
-		Instantiate (obj[Random.Range (0, obj.GetLength(0))], new Vector3(viewpos.x + 5, -4, 0), Quaternion.identity);
+	//	Vector3 viewpos = cam.WorldToViewportPoint(new Vector3());
+		Instantiate (obj[Random.Range (0, obj.GetLength(0))], gameObject.transform.position, Quaternion.identity);
 	}
 }
