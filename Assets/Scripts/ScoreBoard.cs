efde56a946b408e617c284340fe94e0086e692e5
Niina <niinanu@metropolia.fi>
2018-11-20 13:50:30 +0200

    Commented score script

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 
 public class ScoreBoard : MonoBehaviour
 {
+    // making the flieds for the score system
 
     public Text scoreText;
     public Text highScoreText;
@@ -17,7 +18,7 @@ public class ScoreBoard : MonoBehaviour
 
     void Start()
     {
-
+        // takes the high score from the previous achievement and saves it to the high score
         if (PlayerPrefs.HasKey("HighScore"))
         {
             highScoreCount = PlayerPrefs.GetFloat("HighScore");
@@ -28,18 +29,22 @@ public class ScoreBoard : MonoBehaviour
 
     void Update()
     {
-
+        // this increases the score value
         if (scoreIncreasing)
         {
             scoreCount += pointsPerSecond * Time.deltaTime;
         }
 
+        // when the player reaches the highscore this begins to count the score also to the high score
         if (scoreCount > highScoreCount)
         {
             highScoreCount = scoreCount;
+            // the PlayerPrefs saves the high score value when the score reaches the high score
             PlayerPrefs.SetFloat("HighScore", highScoreCount);
         }
 
+        // adding the score values to the scoreText and highScoreText
+        // the Mathf.Round rounds the score value to the closest whole number
         scoreText.text = "Score: " + Mathf.Round(scoreCount);
         highScoreText.text = "High Score: " + Mathf.Round(highScoreCount);
 
