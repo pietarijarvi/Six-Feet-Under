b7beb7e5b0e5cb8f363998557f1223faa4bcef82
Pietari Järvi <penttipj@metropolia.fi>
2018-11-21 12:25:08 +0200

    DB changes

--- Assets/Scripts/LeaderBoardController.cs
+++ Assets/Scripts/LeaderBoardController.cs
@@ -8,8 +8,9 @@ using UnityEngine.UI;
 
 public class LeaderBoardController : MonoBehaviour
 {
-
+    //used for connecting to the database
     private string connection;
+    //List where scores are stored
     private List<Leaderboard> scores = new List<Leaderboard>();
     public GameObject scorePrefab;
     public Transform scoreParent;
@@ -17,97 +18,104 @@ public class LeaderBoardController : MonoBehaviour
     public int saveScores;
     public Text enterName;
 
-    // Use this for initialization
+    /// <summary>
+    /// What happens when the game is started
+    /// </summary>
     void Start()
     {
+        //Connecting to database file in a specific location
         connection = "URI=file:" + Application.dataPath + "/PeliProjektiDB.db";
 
+        //Deleting unneeded scores
         DeleteExtras();
-
+        
+        //Showing scores
         ShowScores();
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    /// <summary>
+    /// Inserting score and name to leaderboard
+    /// </summary>
     public void InsertScore()
     {
+        //If inputfield is not empty
         if (enterName.text != string.Empty)
         {
+            //Score is scoreCount made in ScoreBoard script
             float score = ScoreBoard.scoreCount;
+            //Adding entered name and score that we got in the game
             AddScores(enterName.text, score);
             enterName.text = string.Empty;
             ShowScores();
         }
     }
-
-    public void AddScores(string name, float newScore)
-    {
+    /// <summary>
+    /// Add scores to database
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="theScore"></param>
+    public void AddScores(string name, float theScore)
+    {   //connecting to the database
         using (IDbConnection dbconnection = new SqliteConnection(connection))
-        {
+        {   //opening connection
             dbconnection.Open();
+            //using connection
             using (IDbCommand dbCmd = dbconnection.CreateCommand())
-            {
-                string sqlQuery = String.Format("INSERT INTO HighScores(Name,Score) VALUES(\"{0}\",\"{1}\")", name, newScore);
+            {   //Defining how data is inserted with String.Format
+                string sqlQuery = String.Format("INSERT INTO HighScores(Name,Score) VALUES(\"{0}\",\"{1}\")", name, theScore);
 
                 dbCmd.CommandText = sqlQuery;
+                //Executing the command 
                 dbCmd.ExecuteScalar();
+                //closing connection
                 dbconnection.Close();
             }
         }
     }
-
+    /// <summary>
+    /// Get scores from leaderboard
+    /// </summary>
     void GetScores()
     {
+        //clearing list so we dont show same scores multiple times
         scores.Clear();
+        //connecting to database
         using (IDbConnection dbconnection = new SqliteConnection(connection))
         {
+            //opening connection
             dbconnection.Open();
+            //using connection
             using (IDbCommand dbCmd = dbconnection.CreateCommand())
             {
-                string sqlQuery = "SELECT * FROM HighScores";
-
+                //Selects everything from database table
+                string sqlQuery = "SELECT * FROM HighScores ORDER BY Score DESC";
+                //giving query (db command) a commandtext
                 dbCmd.CommandText = sqlQuery;
-
+                //reading data from database, ExecuteReader executes command we created, reader will have the data from database 
                 using (IDataReader reader = dbCmd.ExecuteReader())
-                {
+                {   //while reader reads
                     while (reader.Read())
-                    {
+                    {   //adding PlayerID, Score and Name with positions in the database (0,1,2)
                         scores.Add(new Leaderboard(reader.GetInt32(0), reader.GetInt32(2), reader.GetString(1)));
 
                     }
+                    //closing connection to database
                     dbconnection.Close();
+                    //closing the reader
                     reader.Close();
                 }
             }
         }
-        scores.Sort();
     }
-
-    void DeleteScores(int id)
-    {
-        using (IDbConnection dbconnection = new SqliteConnection(connection))
-        {
-            dbconnection.Open();
-            using (IDbCommand dbCmd = dbconnection.CreateCommand())
-            {
-                string sqlQuery = String.Format("DELETE FROM HighScores WHERE PlayerID = \"{0}\"", id);
-
-                dbCmd.CommandText = sqlQuery;
-                dbCmd.ExecuteScalar();
-                dbconnection.Close();
-            }
-        }
-    }
-
+    
+    /// <summary>
+    /// Showing our scores
+    /// </summary>
     public void ShowScores()
     {
         GetScores();
-
+        //Where tag is score
         foreach (GameObject score in GameObject.FindGameObjectsWithTag("Score"))
         {
             Destroy(score);
@@ -125,7 +133,9 @@ public class LeaderBoardController : MonoBehaviour
             }
         }
     }
-
+    /// <summary>
+    /// Deleting extra scores from the bottom that we dont need anymore
+    /// </summary>
     void DeleteExtras()
     {
         GetScores();
--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -3,34 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-class Leaderboard:IComparable<Leaderboard>
+class Leaderboard
 {
+    //properties for database data
     public float Score { get; set; }
     public string Name { get; set; }
     public int ID { get; set; }
 
+    //constructor for when a new leaderboard is created
     public Leaderboard(int id, float score, string name)
     {
         this.Score = score;
         this.Name = name;
         this.ID = id;
     }
-
-    public int CompareTo(Leaderboard other)
-    {
-        //first>second return -1
-        //first<second return 1
-        //first==second return 0
-
-        if (other.Score < this.Score)
-        {
-            return -1;
-        }
-
-        else if (other.Score > this.Score)
-        {
-            return 1;
-        }
-        return 0;
-    }
+    
 }
