9c97f346204f646c60eb1372fc92b47ffe17f3bf
Niina <niinanu@metropolia.fi>
2018-11-26 09:46:17 +0200

    People chasing

--- Assets/Scripts/LeaderBoardController.cs
+++ Assets/Scripts/LeaderBoardController.cs
@@ -8,150 +8,101 @@ using UnityEngine.UI;
 
 public class LeaderBoardController : MonoBehaviour
 {
+    //connection variable used to connect to the database
+    private string conn;
+    //texts that show the data in the scene
+    private Text scoreText;
+    private Text nameText;
+    private Text rankText;
+    float score;
+    //SerializeField is used to see the private field in Unity
+    [SerializeField] private Text inputText;
 
-    private string connection;
-    private List<Leaderboard> scores = new List<Leaderboard>();
-    public GameObject scorePrefab;
-    public Transform scoreParent;
-    public int topScores;
-    public int saveScores;
-    public Text enterName;
-
-    // Use this for initialization
+    /// <summary>
+    /// What happens when the game is started
+    /// </summary>
     void Start()
     {
-        connection = "URI=file:" + Application.dataPath + "/PeliProjektiDB.db";
-
-        DeleteExtras();
-
-        ShowScores();
-
+        //Finding text components in gameover scene
+        inputText = GameObject.Find("EnterName").GetComponent<Text>();
+        scoreText = GameObject.Find("Score").GetComponent<Text>();
+        rankText = GameObject.Find("Rank").GetComponent<Text>();
+        nameText = GameObject.Find("Name").GetComponent<Text>();
+        //conn variable is the link to the database inside the asset
+        conn = "URI=file:" + Application.dataPath + "/PeliProjektiDB.db";
+        //Getting scores from database
+        GetScores();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddScore()
     {
-        
-    }
-
-    public void InsertScore()
-    {
-        if (enterName.text != string.Empty)
-        {
-            float score = ScoreBoard.scoreCount;
-            AddScores(enterName.text, score);
-            enterName.text = string.Empty;
-            ShowScores();
-        }
-    }
-
-    public void AddScores(string name, float newScore)
-    {
-        using (IDbConnection dbconnection = new SqliteConnection(connection))
-        {
-            dbconnection.Open();
-            using (IDbCommand dbCmd = dbconnection.CreateCommand())
-            {
-                string sqlQuery = String.Format("INSERT INTO HighScores(Name,Score) VALUES(\"{0}\",\"{1}\")", name, newScore);
+        //score is got from scoreboard class
+        score = ScoreBoard.GetScoreCount();
+        //name is the text input in the inputfield
+        name = inputText.text.ToString();
+        //connecting to the database
+        using (IDbConnection dbconn = new SqliteConnection(conn))
+        {   //opening connection
+            dbconn.Open();
+            //using connection
+            using (IDbCommand dbCmd = dbconn.CreateCommand())
+            {   //Defining how data is inserted with String.Format
+                string sqlQuery = String.Format("INSERT INTO HighScores(Name,Score) VALUES(\"{0}\",\"{1}\")", name, score);
+                
 
                 dbCmd.CommandText = sqlQuery;
+                //Executing the command 
                 dbCmd.ExecuteScalar();
-                dbconnection.Close();
+                //Getting scores again so that leaderboard updates when the button is clicked
+                GetScores();
+                //closing connection
+                dbconn.Close();              
             }
+            
         }
     }
 
-    void GetScores()
+    /// <summary>
+    /// Gets data from the database
+    /// </summary>
+    public void GetScores()
     {
-        scores.Clear();
-        using (IDbConnection dbconnection = new SqliteConnection(connection))
+        //return a string from the database with past player name and their respective score
+        using (IDbConnection dbconn = new SqliteConnection(conn))
         {
-            dbconnection.Open();
-            using (IDbCommand dbCmd = dbconnection.CreateCommand())
+            //opening connection to database
+            dbconn.Open();
+            using (IDbCommand dbCmd = dbconn.CreateCommand())
             {
-                string sqlQuery = "SELECT * FROM HighScores";
-
+                //Select all from highscores table and sort them from highest to lowest
+                string sqlQuery = String.Format("SELECT * FROM HighScores ORDER BY Score DESC");
                 dbCmd.CommandText = sqlQuery;
 
+                nameText.text = "";
+                scoreText.text = "";
+                rankText.text = "";
+                
                 using (IDataReader reader = dbCmd.ExecuteReader())
                 {
-                    while (reader.Read())
+                    int i = 0;
+                    int rank = 1;
+                    //setting database to show only the top 5 scores
+                    while (reader.Read()&& i<5)
                     {
-                        scores.Add(new Leaderboard(reader.GetInt32(0), reader.GetInt32(2), reader.GetString(1)));
-
-                    }
-                    dbconnection.Close();
-                    reader.Close();
-                }
-            }
-        }
-        scores.Sort();
-    }
-
-    void DeleteScores(int id)
-    {
-        using (IDbConnection dbconnection = new SqliteConnection(connection))
-        {
-            dbconnection.Open();
-            using (IDbCommand dbCmd = dbconnection.CreateCommand())
-            {
-                string sqlQuery = String.Format("DELETE FROM HighScores WHERE PlayerID = \"{0}\"", id);
-
-                dbCmd.CommandText = sqlQuery;
-                dbCmd.ExecuteScalar();
-                dbconnection.Close();
-            }
-        }
-    }
-
-    public void ShowScores()
-    {
-        GetScores();
-
-        foreach (GameObject score in GameObject.FindGameObjectsWithTag("Score"))
-        {
-            Destroy(score);
-        }
-
-        for (int i = 0; i < topScores; i++)
-        {
-            if (i <= scores.Count - 1)
-            {
-                GameObject tmp = Instantiate(scorePrefab);
-                Leaderboard tmpScore = scores[i];
-                tmp.GetComponent<ShowingScores>().SetScore(tmpScore.Name, tmpScore.Score.ToString(), '#' + (i + 1).ToString());
-                tmp.transform.SetParent(scoreParent);
-                tmp.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
-            }
-        }
-    }
-
-    void DeleteExtras()
-    {
-        GetScores();
-
-        if (saveScores <= scores.Count)
-        {
-            int deleteCount = scores.Count - saveScores;
-            scores.Reverse();
-
-            using (IDbConnection dbconnection = new SqliteConnection(connection))
-            {
-                dbconnection.Open();
-                using (IDbCommand dbCmd = dbconnection.CreateCommand())
-                {
-                    for (int i = 0; i < deleteCount; i++)
-                    {
-
-
-                        string sqlQuery = String.Format("DELETE FROM HighScores WHERE PlayerID = \"{0}\"", scores[i].ID);
-
-                        dbCmd.CommandText = sqlQuery;
-                        dbCmd.ExecuteScalar();
+                        i++;
+                        //what name and score correlate to in the database ("(1)" is the position for name and "(2)" position for score)
+                        string name = reader.GetString(1);
+                        float score = reader.GetFloat(2);
                         
+                        nameText.text += name.ToString() + "\n";
+                        scoreText.text += score.ToString() + "\n";
+                        rankText.text += rank.ToString() + "\n";
+                        rank++;
                     }
-                    dbconnection.Close();
-                }   
+                    //closing reader and connection
+                    dbconn.Close();
+                    reader.Close();                    
+                }
             }
         }
     }
