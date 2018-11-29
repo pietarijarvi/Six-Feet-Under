5629e0c1f2b248aed5215606f1708f0119a0b10c
Pietari Järvi <penttipj@metropolia.fi>
2018-11-29 11:06:33 +0200

    Pause game

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
+    //making a boolean for game being paused
+    private bool gamePaused;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        // game isn't paused at the start
+        gamePaused = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If escape key is pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //if game isn't paused
+            if (gamePaused == false)
+            {
+                //pauses the game and makes boolean true
+                Time.timeScale = 0;
+                gamePaused = true;
+            }
+            //if boolean true (game is paused) and escape key is pressed
+            else if (gamePaused == true)
+            {
+                //Game unpauses and boolean set back to false
+                Time.timeScale = 1;
+                gamePaused = false;
+            }
+        }
+    }
 }
