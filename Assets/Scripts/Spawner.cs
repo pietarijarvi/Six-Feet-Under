1751c695ae8d19d8ff4a61a9b40af00d32f071e5
Marko Seppänen <marko.p.seppanen@gmail.com>
2018-12-12 09:48:32 +0200

    more tweaks

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour {
 
-	[SerializeField] private float maxSpawn;
+	[SerializeField] private float maxSpawn; 
 	[SerializeField] private float minSpawn;
 	[SerializeField] private GameObject[] obj;
 	void Start () {
-		StartCoroutine(Spawn());
+		StartCoroutine(Spawn()); // Starts coroutine Spawn()
 	}
 	
+	///<summary>
+	/// IEnumerator method for spawning objects from array to be used in conjuction with Unity's Coroutine.
+	///</summary>
 	IEnumerator Spawn()
 	{
+		//Creating infinite while loop for spawning objects from array
 		while (true) {
 			yield return new WaitForSeconds(Random.Range(minSpawn,maxSpawn));
 			Instantiate (obj[Random.Range (0, obj.GetLength(0))], gameObject.transform.position, Quaternion.identity);
